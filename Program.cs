cb219153e0ff8974bd26811b19498f4b31757876
Nicholas <nicholasdev@zohomail.com>
2023-03-03 22:25:54 -0300

    Canção Pastorzinho

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("---Canção Pastorzinho---");
+Console.WriteLine();
+Console.WriteLine("Dó, ré, mi, fá, fá, fá,");
+Console.WriteLine("Dó, ré, dó, ré, ré, ré,");
+Console.WriteLine("Dó, sol, fá, mi, mi, mi,");
+Console.WriteLine("Dó, ré, mi, fá, fá, fá.");
